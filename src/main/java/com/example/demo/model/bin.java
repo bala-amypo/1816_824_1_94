7f02c2dca7ceecf1ed694eadab1c666016819be5
bala-amypo <bala@amypo.in>
2025-12-17 06:50:30 +0000

    User commit

--- src/main/java/com/example/demo/model/bin.java
+++ src/main/java/com/example/demo/model/bin.java
@@ -1,21 +1,28 @@
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.column;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 @Entity
 public class bin
 {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    @column(unique=true)
+    @Column(unique=true)
     private String identifier;
     private String locationDescription;
     private Double latitude;
     private Double longitude;
     private Double capacityLiters;
     private Boolean active;
-
-
 }
-
+public Long getId()
+{
+    return id;
+}
+public Long setId()
+{
+    
+}
