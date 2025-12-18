b7f4a21edac8a891fde62a1cbcdb0097c9fbfa04
bala-amypo <bala@amypo.in>
2025-12-18 05:18:09 +0000

    User commit

--- src/main/java/com/example/demo/repository/binrepository.java
+++ src/main/java/com/example/demo/repository/binrepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.example.demo.model.bin;
 public interface binrepository extends JpaRepository<bin,Long>
 {
 
