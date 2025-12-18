384f81ca7ed09055ce70d6792e03941c3546aa0d
bala-amypo <bala@amypo.in>
2025-12-18 10:14:54 +0000

    User commit

--- src/main/java/com/example/demo/service/FillLevelRecordService.java
+++ src/main/java/com/example/demo/service/FillLevelRecordService.java
@@ -3,12 +3,13 @@ package com.example.demo.service;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.FillLevelRecord;
 import com.example.demo.repository.FillLevelRecordRepository;
+import java.util.List;
 
 @Service
 public interface FillLevelRecordService 
 {
      FillLevelRecord createRecord(FillLevelRecord record);
-     FillLevelRecord getRecordsForBin(Long binId);
+     List<FillLevelRecord> getRecordsForBin(Long binId);
      FillLevelRecord getRecordById(Long id);
-     FillLevelRecord getRecentRecords(Long binId,int limit);
+     List<FillLevelRecord> getRecentRecords(Long binId,int limit);
 }
--- src/main/java/com/example/demo/service/impl/FillLevelRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/FillLevelRecordServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.service.impl;
 
 import com.example.demo.model.FillLevelRecord;
 import com.example.demo.service.FillLevelRecordService;
+import org.springframework.stereotype.Service;
 
 public class FillLevelRecordSeviceImpl implements FillLevelRecordService
 {
