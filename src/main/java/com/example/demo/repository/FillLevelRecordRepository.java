c44765bca62d1e1cff029cdf88105363167242e0
bala-amypo <bala@amypo.in>
2025-12-18 08:18:59 +0000

    User commit

--- src/main/java/com/example/demo/repository/FillLevelRecordRepository.java
+++ src/main/java/com/example/demo/repository/FillLevelRecordRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.FillLevelRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FillLevelRecordRepository extends JpaRepository<FillLevelRecord,Long>
 {
