923a153cc1564835d8e2c1061b8531c4d432dd26
bala-amypo <bala@amypo.in>
2025-12-18 06:08:07 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/BinServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BinServiceImpl.java
@@ -1,7 +1,15 @@
 import org.springframework.stereotype.Service;
-
+import com.example.demo.model.Bin;
+import com.example.demo.repository.BinRepository;
+import com.example.demo.service.BinService;
+import 
 @Service
 public class BinServiceImpl implements BinService
 {
-   public 
+   @Autowired
+   Bin 
+   public Bin createBin(Bin bin)
+   {
+      
+   }
 }
