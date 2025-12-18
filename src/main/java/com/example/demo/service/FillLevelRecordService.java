c65d799342e88021d7a24d9681636649e3e98806
bala-amypo <bala@amypo.in>
2025-12-18 08:38:59 +0000

    User commit

--- src/main/java/com/example/demo/service/FillLevelRecordService.java
+++ src/main/java/com/example/demo/service/FillLevelRecordService.java
@@ -1,8 +1,11 @@
 package com.example.demo.service;
+
 import org.springframework.stereotype.Service;
+import com.example.demo.model.FillLevelRecord;
+import com.example.demo.repository.FillLevelRecordRepository;
 
 @Service
 public interface FillLevelRecordService 
 {
-
+    public FillLevelRecord createRecord(FillLevelRecord )
 }
