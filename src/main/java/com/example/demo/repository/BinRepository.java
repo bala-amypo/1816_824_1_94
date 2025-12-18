0f1a0d0cf8519fb26b0f4fdeeb91d4dfcc2f9fe3
bala-amypo <bala@amypo.in>
2025-12-18 08:13:59 +0000

    User commit

--- src/main/java/com/example/demo/repository/BinRepository.java
+++ src/main/java/com/example/demo/repository/BinRepository.java
@@ -3,5 +3,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.Bin;
 public interface BinRepository extends JpaRepository<Bin,Long>
 {
-
+    
 }
---
+++ src/main/java/com/example/demo/repository/FillLevelRecordRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.FillLevelRecord;
+
+public interface FillLevelRecordRepository extends JpaRepository<FillLevelRecord,Long>
+{
+
+}
