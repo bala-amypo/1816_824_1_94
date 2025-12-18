7e30eaee34d7a5ce3cd9c68aec69087bf0653d41
bala-amypo <bala@amypo.in>
2025-12-18 18:31:22 +0000

    User commit

--- src/main/java/com/example/demo/repository/UsagePatternModelRepository.java
+++ src/main/java/com/example/demo/repository/UsagePatternModelRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
-import com.example.demo.
+import com.example.demo.model.UsagePatternModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsagePatternModelRepository extends JpaRepository<UsagePatternModel,Long>
 {
-
+  
 }
