93d4f98cc9903f372a145c2757082ba92e6977b2
bala-amypo <bala@amypo.in>
2025-12-18 04:04:00 +0000

    User commit

--- src/main/java/com/example/demo/model/filllevelrecord.java
+++ src/main/java/com/example/demo/model/filllevelrecord.java
@@ -1,6 +1,35 @@
-import jakarat
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
 public class filllevelrecord
 {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private Double fillPercentage;
+    private Boolean isWeekend;
+    public Long getid()
+    {
+        return id;
+    }
+    public void setid(Long id)
+    {
+        this.id=id;
+    }
+    public Double getfillPercentage()
+    {
+        return id;
+    }
+    public void setfillPercentage()
+    {
+        this.id=id;
+    }
+    public Long getid()
+    {
+        return id;
+    }
+    public void setid(LOng id)
+    {
+        this.id=id;
+    }
 }
