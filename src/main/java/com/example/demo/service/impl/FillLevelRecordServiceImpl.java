ee3d4708ef78be8bb15af2a9762d4f362ce5bdd5
bala-amypo <bala@amypo.in>
2025-12-18 10:19:54 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/FillLevelRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/FillLevelRecordServiceImpl.java
@@ -3,8 +3,10 @@ package com.example.demo.service.impl;
 import com.example.demo.model.FillLevelRecord;
 import com.example.demo.service.FillLevelRecordService;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class FillLevelRecordSeviceImpl implements FillLevelRecordService
 {
-    
+    @Autowired
+   
 }
