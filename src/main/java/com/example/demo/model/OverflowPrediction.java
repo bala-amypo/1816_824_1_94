3fd5ea3280702ec9ce33af789c50d3e6bffe98cc
bala-amypo <bala@amypo.in>
2025-12-18 17:55:22 +0000

    User commit

--- src/main/java/com/example/demo/model/OverflowPrediction.java
+++ src/main/java/com/example/demo/model/OverflowPrediction.java
@@ -2,9 +2,13 @@ import com.example.demo.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Column;
-import jakarta.persistence.
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 public class OverflowPrediction
 {
-    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private LO id;
+    Double 
 }
