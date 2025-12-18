bf099646d1a5d7f9eab3293d058aca04aaff6766
bala-amypo <bala@amypo.in>
2025-12-18 09:44:58 +0000

    User commit

--- src/main/java/com/example/demo/controller/BinController.java
+++ src/main/java/com/example/demo/controller/BinController.java
@@ -1,8 +1,9 @@
-package com.example.demo.contoller;
+package com.example.demo.controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.model.Bin;
 import com.example.demo.service.BinService;
+import java.util.List;
 
 @RestController
 public class BinController
