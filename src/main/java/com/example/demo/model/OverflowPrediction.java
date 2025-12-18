ac8d32ca8e81c3278bf5d8dfd7655d2d8bd75c99
bala-amypo <bala@amypo.in>
2025-12-18 17:52:23 +0000

    User commit

--- src/main/java/com/example/demo/model/OverflowPrediction.java
+++ src/main/java/com/example/demo/model/OverflowPrediction.java
@@ -1,4 +1,8 @@
-import 
+import com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.
 
 public class OverflowPrediction
 {
