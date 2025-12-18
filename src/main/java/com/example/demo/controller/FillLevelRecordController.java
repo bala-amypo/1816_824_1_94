5030164676e7fa74f9f60597da11bc75cf278788
bala-amypo <bala@amypo.in>
2025-12-18 17:19:23 +0000

    User commit

--- src/main/java/com/example/demo/controller/FillLevelRecordController.java
+++ src/main/java/com/example/demo/controller/FillLevelRecordController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.FillLevelRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-
+@RestController
 public class FillLevelRecordController
 {
-
+   @
 }
