708a9e3aa63bb713888544af032b6d3285a14d93
bala-amypo <bala@amypo.in>
2025-12-26 11:28:39 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
@@ -1,6 +1,23 @@
 package com.example.demo.security;
 
 import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.ServletException;
+
+import java.io.IOException;
 
 public class JwtAuthenticationFilter implements Filter {
+
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain
+    ) throws IOException, ServletException {
+
+        // NO JWT LOGIC — just pass request forward
+        chain.doFilter(request, response);
+    }
 }
